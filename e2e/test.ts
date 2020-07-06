f305bc414de0a27252a21fa1994daf2ec89caec9
KHeo <sainthkh@naver.com>
2020-07-06 17:08:53 +0900

    Remove unnecessary test file.

--- e2e/test.ts
+++
@@ -1,8 +0,0 @@
-beforeAll( async () => {
-	await page.goto( 'https://whatismybrowser.com/' );
-} );
-
-it( 'should display "google" text on page', async () => {
-	const browser = await page.$eval( '.string-major', ( el ) => el.innerHTML );
-	expect( browser ).toContain( 'Chrome' );
-} );
