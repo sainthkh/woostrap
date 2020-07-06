be20e3f423a3d9cf7c7af911324c94695871efa7
KHeo <sainthkh@naver.com>
2020-07-07 07:23:19 +0900

    Fix lint.

--- woo-playwright-utils/plugin.php
+++ woo-playwright-utils/plugin.php
@@ -1,13 +1,19 @@
 <?php
 /**
  * Plugin Name: WooCommerce Playwright Utils
+ * 
+ * @package woostrap
  */
 
 add_action( 'init', 'woocommerce_clear_cart_url' );
 
-// Credits to https://gist.github.com/kloon/4541017
+/**
+ * Clear cart with ?clear-cart in the url.
+ * 
+ * Credits to https://gist.github.com/kloon/4541017
+ */ 
 function woocommerce_clear_cart_url() {
-	if ( isset( $_GET['clear-cart'] ) ) {
+	if ( isset( $_GET['clear-cart'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		WC()->cart->empty_cart();
 	}
 }
