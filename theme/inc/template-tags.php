743bb401cb9f720b64f8fcabff34ee1fc45fc401
KHeo <sainthkh@naver.com>
2020-07-16 18:20:23 +0900

    Fix alignment of icons when there is no menu.

--- theme/inc/template-tags.php
+++ theme/inc/template-tags.php
@@ -74,7 +74,7 @@ function woostrap_site_logo( $args = array(), $echo = true ) {
  * Search form for products.
  */
 function woostrap_search_form() { ?>
-	<form role="search" method="get" class="d-none d-md-block form-inline my-2 my-lg-0 woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<form role="search" method="get" class="d-none d-md-block ml-auto my-2 my-lg-0 form-inline woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'woocommerce' ); ?></label>
 		<div class="form-group has-search">
 			<i class="fas fa-search form-control-feedback"></i>
