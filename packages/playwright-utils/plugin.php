41c29e7460502fa4c086233041e529f78ee4949c
KHeo <sainthkh@naver.com>
2020-07-25 12:51:53 +0900

    Add prefixes.

--- packages/playwright-utils/plugin.php
+++ packages/playwright-utils/plugin.php
@@ -5,25 +5,25 @@
  * @package woostrap
  */
 
-add_action( 'init', 'woocommerce_clear_cart_url' );
+add_action( 'init', 'e2e_util_woocommerce_clear_cart_url' );
 
 /**
  * Clear cart with ?clear-cart in the url.
  * 
  * Credits to https://gist.github.com/kloon/4541017
  */ 
-function woocommerce_clear_cart_url() {
+function e2e_util_woocommerce_clear_cart_url() {
 	if ( isset( $_GET['clear-cart'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		WC()->cart->empty_cart();
 	}
 }
 
-add_action('init', 'reset_customize');
+add_action('init', 'e2e_util_reset_customize');
 
 /**
  * Reset wp_customize 
  */
-function reset_customize() {
+function e2e_util_reset_customize() {
 	if ( isset( $_GET['reset-customize'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		remove_theme_mods();
 	}
