5a75c0623ad01b6310f582063948dd477faebb0e
KHeo <sainthkh@naver.com>
2020-07-25 19:08:04 +0900

    Fix lint.

--- packages/playwright-utils/plugin.php
+++ packages/playwright-utils/plugin.php
@@ -18,7 +18,7 @@ function e2e_util_woocommerce_clear_cart_url() {
 	}
 }
 
-add_action('init', 'e2e_util_reset_customize');
+add_action( 'init', 'e2e_util_reset_customize' );
 
 /**
  * Reset wp_customize 
@@ -29,28 +29,31 @@ function e2e_util_reset_customize() {
 	}
 }
 
-add_action('init', 'e2e_util_logout');
+add_action( 'init', 'e2e_util_logout' );
 
+/**
+ * Logout user
+ */
 function e2e_util_logout() {
 	if ( isset( $_GET['logout'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		wp_logout();
 	}
 }
 
-add_action('init', 'e2e_util_login');
+add_action( 'init', 'e2e_util_login' );
 
+/**
+ * Login user
+ */
 function e2e_util_login() {
 	if ( isset( $_GET['login'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$username = $_GET['login'];
+		$username = wp_kses_data( wp_unslash( $_GET['login'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$user     = get_user_by( 'login', $username );
 
-		$user = get_user_by('login', $username );
-
-		// Redirect URL //
-		if ( !is_wp_error( $user ) )
-		{
+		if ( ! is_wp_error( $user ) ) {
 			wp_clear_auth_cookie();
-			wp_set_current_user ( $user->ID );
-			wp_set_auth_cookie  ( $user->ID );
+			wp_set_current_user( $user->ID );
+			wp_set_auth_cookie( $user->ID );
 		}
 	}
 }
