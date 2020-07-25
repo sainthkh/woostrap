87a78614d1744a8e24641b5fd2ec8d0f684ea994
KHeo <sainthkh@naver.com>
2020-07-26 06:56:38 +0900

    Test url.

--- packages/e2e-tests/src/customizer/header.ts
+++ packages/e2e-tests/src/customizer/header.ts
@@ -9,33 +9,32 @@ beforeEach( async () => {
 
 describe( 'site identity', () => {
 	it( 'retina logo', async () => {
-		await page.click( '#accordion-section-title_tagline', {
-			timeout: 0,
-		} );
-		await page.click( '//button[text()="Select logo"]' );
-		await page.click( '//button[text()="Media Library"]' );
-		await page.click( 'ul.attachments li' );
-
-		const originalSize = await page.innerText( '.dimensions' );
-		const originalHeight = originalSize.match( /\d+/g )[ 1 ];
-
-		await page.click( '//button[text()="Select"]' );
-		await page.click( '//button[text()="Skip Cropping"]' );
-		await page.click( '#_customize-input-retina_logo' );
-		await page.click( '#save' );
-		await page.waitForResponse(
-			'http://localhost:8889/wp-admin/admin-ajax.php'
-		);
-		await page.click( 'a.customize-controls-close' );
-
-		const style = await page.getAttribute(
-			'a.custom-logo-link img',
-			'style'
+		expect( page.url() ).toBe(
+			'http://localhost:8889/wp-admin/customize.php'
 		);
 
-		const halfHeight = `height: ${ parseInt( originalHeight ) / 2 }px`;
-
-		expect( style ).toContain( halfHeight );
+		// await page.click( '#accordion-section-title_tagline', {
+		// 	timeout: 0,
+		// } );
+		// await page.click( '//button[text()="Select logo"]' );
+		// await page.click( '//button[text()="Media Library"]' );
+		// await page.click( 'ul.attachments li' );
+		// const originalSize = await page.innerText( '.dimensions' );
+		// const originalHeight = originalSize.match( /\d+/g )[ 1 ];
+		// await page.click( '//button[text()="Select"]' );
+		// await page.click( '//button[text()="Skip Cropping"]' );
+		// await page.click( '#_customize-input-retina_logo' );
+		// await page.click( '#save' );
+		// await page.waitForResponse(
+		// 	'http://localhost:8889/wp-admin/admin-ajax.php'
+		// );
+		// await page.click( 'a.customize-controls-close' );
+		// const style = await page.getAttribute(
+		// 	'a.custom-logo-link img',
+		// 	'style'
+		// );
+		// const halfHeight = `height: ${ parseInt( originalHeight ) / 2 }px`;
+		// expect( style ).toContain( halfHeight );
 	} );
 } );
 
