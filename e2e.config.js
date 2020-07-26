1a79990ed2ff54b40585afa51156a588ef02ade2
Kukhyeon Heo <sainthkh@naver.com>
2020-07-26 18:38:47 +0900

    Add e2e tests for customizer site identity. (#15)
    * Reset customize.
    
    * Add prefixes.
    
    * Add login and logout functions.
    
    * Reorganize files.
    
    * Add test for retina_logo option.
    
    * Fix lint.
    
    * Increase jest timeout to 1 minute
    
    * Fix timeout.
    
    * disable timeout.
    
    * Test url.
    
    * Add slash.
    
    * Wait for wpadminbar
    
    * You should login with login form at least once.
    
    * Revive tests.

--- e2e.config.js
+++ e2e.config.js
@@ -1,11 +1,14 @@
 module.exports = {
 	preset: 'jest-playwright-preset',
-	testMatch: [ '**/packages/e2e-tests/**/*.[jt]s?(x)' ],
+	testMatch: [ '**/packages/e2e-tests/src/**/*.[jt]s?(x)' ],
 	testPathIgnorePatterns: [
 		'/node_modules/',
 		'/wordpress/',
 		'/vendor',
 		'index.d.ts',
+	],
+	setupFilesAfterEnv: [
+		'<rootDir>/packages/e2e-tests/config/setup-test-framework.js',
 	],
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
--- packages/playwright-utils/plugin.php
+++ packages/playwright-utils/plugin.php
@@ -5,15 +5,55 @@
  * @package woostrap
  */
 
-add_action( 'init', 'woocommerce_clear_cart_url' );
+add_action( 'init', 'e2e_util_woocommerce_clear_cart_url' );
 
 /**
  * Clear cart with ?clear-cart in the url.
  * 
  * Credits to https://gist.github.com/kloon/4541017
  */ 
-function woocommerce_clear_cart_url() {
+function e2e_util_woocommerce_clear_cart_url() {
 	if ( isset( $_GET['clear-cart'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		WC()->cart->empty_cart();
+	}
+}
+
+add_action( 'init', 'e2e_util_reset_customize' );
+
+/**
+ * Reset wp_customize 
+ */
+function e2e_util_reset_customize() {
+	if ( isset( $_GET['reset-customize'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		remove_theme_mods();
+	}
+}
+
+add_action( 'init', 'e2e_util_logout' );
+
+/**
+ * Logout user
+ */
+function e2e_util_logout() {
+	if ( isset( $_GET['logout'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		wp_logout();
+	}
+}
+
+add_action( 'init', 'e2e_util_login' );
+
+/**
+ * Login user
+ */
+function e2e_util_login() {
+	if ( isset( $_GET['login'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$username = wp_kses_data( wp_unslash( $_GET['login'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$user     = get_user_by( 'login', $username );
+
+		if ( ! is_wp_error( $user ) ) {
+			wp_clear_auth_cookie();
+			wp_set_current_user( $user->ID );
+			wp_set_auth_cookie( $user->ID );
+		}
 	}
 }
